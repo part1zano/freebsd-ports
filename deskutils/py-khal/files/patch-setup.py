70dcc68e3ae00c3f98ef33b2fd48f6dea6e85069
Maxim V Filimonov <che@bein.link>
2016-10-08 17:27:35 +0300

    Reverted khal update: khal 0.8 doesnt support py2

--- deskutils/py-khal/files/patch-setup.py
+++ deskutils/py-khal/files/patch-setup.py
@@ -1,9 +1,9 @@
---- setup.py.orig	2016-10-06 17:02:45 UTC
+--- setup.py.orig	2016-01-05 12:53:52 UTC
 +++ setup.py
-@@ -43,8 +43,6 @@ setup(
+@@ -40,8 +40,6 @@ setup(
+     },
      install_requires=requirements,
      extras_require=extra_requirements,
-     tests_require=test_requirements,
 -    setup_requires=['setuptools_scm'],  # not needed when using packages from PyPI
 -    use_scm_version={'write_to': 'khal/version.py'},
      classifiers=[
